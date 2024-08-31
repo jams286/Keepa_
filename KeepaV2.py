f1eb65792856abc18b261c9628ebe380c639cbf3
jams286 <jaime286tm@gmail.com>
2024-08-30 23:39:19 -0600

    async function

--- KeepaV2.py
+++ KeepaV2.py
@@ -7,7 +7,12 @@ config = KUtils.getConfig()
 url_base = config['keepa']['url']
 api_key = config['keepa']['api_key']    
 api = keepa.Keepa(api_key)
-api.best_sellers_query
+# api.best_sellers_query
+
+async def GetProductos(asins:list, domain:str, buybox:bool, days:int) :
+    api_async = await keepa.AsyncKeepa.create(api_key)
+    return await api_async.query(asins, domain=domain, buybox=buybox, days=days)
+    
 
 def BestSellers(domain:int, category:str, range:int, mont:int, year:int)->list:
     asins_l = []
@@ -31,16 +36,17 @@ def BestSellers(domain:int, category:str, range:int, mont:int, year:int)->list:
 
 
 if __name__ == '__main__':
+    pass
     #BestSellers US
-    bs_us_l = BestSellers(1, '165793011',30,11,2023)
-    #BestSellers CA
-    bs_ca_l = BestSellers(6,'165793011',30,11,2023)
-    KUtils.SaveBestSellerList(bs_us_l, '165793011',1,'2023-11')
-    KUtils.SaveBestSellerList(bs_ca_l, '165793011',6,'2023-11')
+    # bs_us_l = BestSellers(1, '165793011',30,11,2023)
+    # #BestSellers CA
+    # bs_ca_l = BestSellers(6,'165793011',30,11,2023)
+    # KUtils.SaveBestSellerList(bs_us_l, '165793011',1,'2023-11')
+    # KUtils.SaveBestSellerList(bs_ca_l, '165793011',6,'2023-11')
 
-    #BestSellers US
-    bs_us_l = BestSellers(1, '165793011',30,12,2023)
-    #BestSellers CA
-    bs_ca_l = BestSellers(6,'165793011',30,12,2023)
-    KUtils.SaveBestSellerList(bs_us_l, '165793011',1,'2023-12')
-    KUtils.SaveBestSellerList(bs_ca_l, '165793011',6,'2023-12')
+    # #BestSellers US
+    # bs_us_l = BestSellers(1, '165793011',30,12,2023)
+    # #BestSellers CA
+    # bs_ca_l = BestSellers(6,'165793011',30,12,2023)
+    # KUtils.SaveBestSellerList(bs_us_l, '165793011',1,'2023-12')
+    # KUtils.SaveBestSellerList(bs_ca_l, '165793011',6,'2023-12')
